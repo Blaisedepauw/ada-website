e18f6475bac220a01b389bc7b9138c0512868ab0
Blaise <blaise.depauw@student-cs.fr>
2024-12-09 14:22:29 +0100

    ajouter les titres

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,20 +1,27 @@
+function updateHeader() {
+  const fullscreenImage = document.getElementById('fullscreenImage');
+  const pageIndex = document.getElementById('page-index');
+
+  if (window.scrollY > 0) {
+    fullscreenImage.classList.add('header-image');
+    pageIndex.classList.remove('hidden');
+  } else {
+    fullscreenImage.classList.remove('header-image');
+    pageIndex.classList.add('hidden');
+  }
+}
+
+// Check scroll position when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+  updateHeader();
+});
+
+// Check scroll position when scrolling
 window.addEventListener('scroll', function() {
-    const fullscreenImage = document.getElementById('fullscreenImage');
-    const pageIndex = document.getElementById('page-index');
+  updateHeader();
+});
 
-    if (window.scrollY > 0) {
-      fullscreenImage.classList.add('header-image');
-      pageIndex.classList.remove('hidden'); // Show the index after scrolling
-    } else {
-      fullscreenImage.classList.remove('header-image');
-      pageIndex.classList.add('hidden'); // Hide the index when at the top
-    }
-  });
-
-  // Initial check in case the page is loaded with a scroll position
-  document.addEventListener('DOMContentLoaded', function() {
-    const pageIndex = document.getElementById('page-index');
-    if (window.scrollY === 0) {
-      pageIndex.classList.add('hidden');
-    }
-  });
+// Check scroll position when the window is resized
+window.addEventListener('resize', function() {
+  updateHeader();
+});
